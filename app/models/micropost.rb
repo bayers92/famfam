4287772478609b8660abdf467a3668293a6cfce7
bayers92 <brianc.ayers@gmail.com>
2014-12-12 18:36:24 -0500

    more styling

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,5 @@ class Micropost < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_attached_file :pic, :styles => { :large => "700x700", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
  	validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+ 	default_scope -> { order(created_at: :desc) }
 end
