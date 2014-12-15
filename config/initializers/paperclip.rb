e9e7235089f278559e267f9a3d5ff02f2be68476
bayers92 <brianc.ayers@gmail.com>
2014-12-15 13:22:35 -0500

    testign

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
-Paperclip::Attachment.default_options[:url] = 'famfamfambucket.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'famfamfambucket.s3.amazonaws.com' 
+if Rails.env.production?
+	Paperclip::Attachment.default_options[:url] = 'famfamfambucket.s3.amazonaws.com'
+	Paperclip::Attachment.default_options[:path] = '/:attachment/:id_partition/:style/:filename'
+	Paperclip::Attachment.default_options[:s3_host_name] = 'famfamfambucket.s3.amazonaws.com'
+end
