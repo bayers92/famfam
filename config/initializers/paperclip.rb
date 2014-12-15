c905951c7bb50b255b9b86cf3fffc4f4e5c981c3
bayers92 <brianc.ayers@gmail.com>
2014-12-15 13:26:53 -0500

    region path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-	Paperclip::Attachment.default_options[:url] = 'famfamfambucket.s3.amazonaws.com'
-	Paperclip::Attachment.default_options[:path] = '/:attachment/:id_partition/:style/:filename'
-	Paperclip::Attachment.default_options[:s3_host_name] = 'famfamfambucket.s3.amazonaws.com'
+	Paperclip::Attachment.default_options[:url] = 's3-us-west-1.amazonaws.com'
+	Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+	Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
 end
