a60481f0f374330bc21189ceddcf3e7fa381020d
bayers92 <brianc.ayers@gmail.com>
2014-12-15 11:03:42 -0500

    testing aws initiliazer

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -14,6 +14,7 @@ class MicropostsController < ApplicationController
 
   # GET /microposts/new
   def new
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @micropost = Micropost.new
   end
 
