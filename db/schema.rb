cdb1cc728ed95a5785369ff451f93e9fddb4a106
bayers92 <brianc.ayers@gmail.com>
2014-12-17 09:17:49 -0500

    finished comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212031740) do
+ActiveRecord::Schema.define(version: 20141217140824) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141212031740) do
     t.integer  "micropost_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["micropost_id"], name: "index_comments_on_micropost_id"
